c74bdc6230f24bc59b300ddf835d842a2712fb3f
Your Name <you@example.com>
2017-02-04 14:47:33 +0300

    guessing-game/feature: implement methods constructor, setRange, guess, lower, greater.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min_num = 0;
+        this.max_num = 0;
+        this.final = 0;
+
+    }
 
     setRange(min, max) {
-
+        this.min_num = min;
+        this.max_num = max;
     }
 
     guess() {
-
+        this.final = Math.ceil((this.min_num+this.max_num)/2);
+        return this.final;
     }
 
     lower() {
-
+        this.max_num = this.final;
+        return this.max_num;
     }
 
     greater() {
-
+        this.min_num = this.final;
+       return this.min_num;
     }
 }
 
